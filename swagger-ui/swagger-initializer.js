a5030946103238dc161a3c11972de6282fbf4afe
John Andrews <john@reven.nz>
2022-07-14 00:43:31 +1200

    docs

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -3,9 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/swagger-ui/swagger.json",
+    url: "https://docs.fileflows.com/swagger-ui/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    tryItOutEnabled: false,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
